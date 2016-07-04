c73c5d8c115f1f7d765577b81cc9f3f49fe5f0d5
Michael James Davies <mdavies2857@gmail.com>
2016-07-04 09:30:48 +0000

    Making sure Week 3, day 2, Ruby exercise was done correctly.

---
+++ actions.rb
@@ -0,0 +1,68 @@
+def humanized_time_ago(time_ago_in_minutes)
+    if time_ago_in_minutes >= 60
+        "#{time_ago_in_minutes / 60} hours ago"
+    else
+        "#{time_ago_in_minutes} minutes ago"
+    end 
+end
+
+get "/" do
+    # Array
+    post_shark = {
+        username: "sharky_j"
+        avatar_url: "http://naserca.com/images/sharky_j.jpg",
+        photo_url: "http://naserca.com/images/shark.jpg",
+        humanized_time_ago: humanized_Time_ago(15),
+        time_ago_in_minutes: 15,
+        like_count: 0,
+        comment_count: 1,
+        comments: [{
+            username: "sharky_j",    
+            text: "sharky_j: out for the weekend... looking for new friends!"
+            }]
+        }
+    
+        post_whale = {
+        username: "kirk_whalum",
+        avatar_url: "http://naserca.com/images/kirk_whalum.jpg",
+        photo_url: "http://naserca.com/imageswhale.jpg",
+        humanized_time_ago: humanized_Time_ago(65),
+        like_count: 0,
+        comment_count: 1,
+        comments: [{
+            username: "kirk_whalum",    
+            text: "#weekendvibes"
+            }]
+        }
+    
+        
+        post_marlin = {
+        username: "marlin_peppa",
+        avatar_url: "http://naserca.com/images/marlin_peppa.jpg",
+        photo_url: "http://naserca.com/images/marlin.jpg",
+        humanized_time_ago: humanized_Time_ago(190),
+        like_count: 0,
+        comment_count: 1,
+        comments: [{
+            username: "marlin_peppa",    
+            text: "lunchtime! ;)"
+            }]
+         }
+        
+        [post_shark,post_whale, post_marlin].to_s
+    end
+    
+    
+        #if the time_ago_in_minutes is more than 60
+        #if time_ago_in_minutes >= 60
+                #return this string
+            #   "#{time_ago_in_minutes / 60}" hours ago"
+            ##if it's an hour exactly
+            #elsifit just happened
+                #   "just a moment ago"
+            #if it's less than or equal to an hour-
+            #else
+                ##return this instead
+                    #"less than one hour"
+                #"#{time_ago_in_minutes} minutes ago"
+            #end
