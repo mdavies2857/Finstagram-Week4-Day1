72878ee64413e695ebf48aa82a9b852b101d5a41
Michael James Davies <mdavies2857@gmail.com>
2016-07-05 05:55:07 +0000

    Finished templating with ruby and ERB exercise

--- app/actions.rb
+++ app/actions.rb
@@ -7,51 +7,50 @@ def humanized_time_ago(time_ago_in_minutes)
 end
     
 get '/' do
-    # Array
-    @post_shark = {
-        username: "sharky_j",
-        avatar_url: "http://naserca.com/images/sharky_j.jpg",
-        photo_url: "http://naserca.com/images/shark.jpg",
-        humanized_time_ago: humanized_time_ago(15),
-        time_ago_in_minutes: 15,
-        like_count: 0,
-        comment_count: 1,
-        comments: [{
-            username: "sharky_j",    
-            text: "sharky_j: out for the weekend... looking for new friends!"
-            }]
-        }
+  # Array
+  @post_shark = {
+    username: "sharky_j",
+    avatar_url: "http://naserca.com/images/sharky_j.jpg",
+    photo_url: "http://naserca.com/images/shark.jpg",
+    humanized_time_ago: humanized_time_ago(15),
+    like_count: 0,
+    comment_count: 1,
+    comments: [{
+      username: "sharky_j",    
+      text: "sharky_j: out for the weekend... looking for new friends!"
+    }]
+  }
     
-    @post_whale = {
-        username: "kirk_whalum",
-        avatar_url: "http://naserca.com/images/kirk_whalum.jpg",
-        photo_url: "http://naserca.com/images/whale.jpg",
-        humanized_time_ago: humanized_time_ago(65),
-        like_count: 0,
-        comment_count: 1,
-        comments: [{
-            username: "kirk_whalum",    
-            text: "#weekendvibes"
-            }]
-        }
+  @post_whale = {
+    username: "kirk_whalum",
+    avatar_url: "http://naserca.com/images/kirk_whalum.jpg",
+    photo_url: "http://naserca.com/images/whale.jpg",
+    humanized_time_ago: humanized_time_ago(65),
+    like_count: 0,
+    comment_count: 1,
+    comments: [{
+      username: "kirk_whalum",    
+      text: "#weekendvibes"
+    }]
+  }
     
         
-    @post_marlin = {
-        username: "marlin_peppa",
-        avatar_url: "http://naserca.com/images/marlin_peppa.jpg",
-        photo_url: "http://naserca.com/images/marlin.jpg",
-        humanized_time_ago: humanized_time_ago(190),
-        like_count: 0,
-        comment_count: 1,
-        comments: [{
-            username: "marlin_peppa",    
-            text: "lunchtime! ;)"
-            }]
-         }
+  @post_marlin = {
+    username: "marlin_peppa",
+    avatar_url: "http://naserca.com/images/marlin_peppa.jpg",
+    photo_url: "http://naserca.com/images/marlin.jpg",
+    humanized_time_ago: humanized_time_ago(190),
+    like_count: 0,
+    comment_count: 1,
+    comments: [{
+      username: "marlin_peppa",    
+      text: "lunchtime! ;)"
+    }]
+   }
 
-    [@post_shark, @post_whale, @post_marlin].to_s
+  @posts = [@post_shark, @post_whale, @post_marlin]
         
-        erb(:index)  
-    end
+   erb(:index)  
+end
     
 
